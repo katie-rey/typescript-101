81dd8c2e753c5584554513294de5a2b77afd4016
Katie-Rey <katiejorey@gmail.com>
2022-07-29 11:53:18 +1200

    first commit

---
+++ basics.js
@@ -0,0 +1,4 @@
+console.log('Hello world!');
+console.log('And goodbye!');
+Math.round(2.334445);
+var pi = 3.141592653589793;
---
+++ basics.ts
@@ -0,0 +1,4 @@
+console.log('Hello world!')
+console.log('And goodbye!')
+Math.round(2.334445)
+let pi: number = 3.141592653589793
