1f769d5ba8ccf61104a322e3da979c85eae951b6
Katie-Rey <katiejorey@gmail.com>
2022-08-02 13:16:49 +1200

    Update compiled file

--- basics.js
+++ basics.js
@@ -1,6 +1,5 @@
-console.log('Hello world!')
-console.log('And goodbye!')
-console.log('Testing')
-
-Math.round(2.334445)
-var pi = 3.141592653589793
+console.log('Hello world!');
+console.log('And goodbye!');
+Math.round(2.334445);
+var pi = 3.141592653589793;
+var name = 'Katie';
