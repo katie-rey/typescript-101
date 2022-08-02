789d999dc0d33923ec43af4854b8e95eda26cc6d
Katie-Rey <katiejorey@gmail.com>
2022-08-02 13:15:24 +1200

    Add variable

--- basics.js
+++ basics.js
@@ -1,4 +1,6 @@
-console.log('Hello world!');
-console.log('And goodbye!');
-Math.round(2.334445);
-var pi = 3.141592653589793;
+console.log('Hello world!')
+console.log('And goodbye!')
+console.log('Testing')
+
+Math.round(2.334445)
+var pi = 3.141592653589793
--- basics.ts
+++ basics.ts
@@ -2,3 +2,4 @@ console.log('Hello world!')
 console.log('And goodbye!')
 Math.round(2.334445)
 let pi: number = 3.141592653589793
+let name: string = 'Katie'
