d1128e5ce00df436b0c9cb012417ac9cf8198db7
Katie-Rey <katiejorey@gmail.com>
2022-08-04 15:55:15 +1200

    Union types exercise

--- Types/unionTypes.ts
+++ Types/unionTypes.ts
@@ -1,57 +1,54 @@
 // Basic Union Type:
-let age: number | string = 21;
-age = 23;
-age = "24";
+let age: number | string = 21
+age = 23
+age = '24'
 
 type Point = {
-  x: number;
-  y: number;
-};
+  x: number
+  y: number
+}
 
 type Loc = {
-  lat: number;
-  long: number;
-};
+  lat: number
+  long: number
+}
 
 // Union type with type aliases
-let coordinates: Point | Loc = { x: 1, y: 34 };
-coordinates = { lat: 321.213, long: 23.334 };
+let coordinates: Point | Loc = { x: 1, y: 34 }
+coordinates = { lat: 321.213, long: 23.334 }
 
 // Function parameter union type:
 function printAge(age: number | string): void {
-  console.log(`You are ${age} years old`);
+  console.log(`You are ${age} years old`)
 }
 
 function calculateTax(price: number | string, tax: number) {
-  if (typeof price === "string") {
-    price = parseFloat(price.replace("$", ""));
+  if (typeof price === 'string') {
+    price = parseFloat(price.replace('$', ''))
   }
-  return price * tax;
+  return price * tax
 }
 
 // const nums: number[] = [1,2,3,4]
 // const stuff: any[] = [1,2,3,4, true, "asdas", {}]
 
+// union type followed by square brackets >>
+
 // const stuff: (number | string)[] = [1,2,3, "das"]
+
+// error below... an array of ALL numbers or ALL strings...
 // const stuff: number[] | string[] = ["asd", "asd", 1]
 
 // Union Type With Arrays
-const coords: (Point | Loc)[] = [];
-coords.push({ lat: 321.213, long: 23.334 });
-coords.push({ x: 213, y: 43 });
+const coords: (Point | Loc)[] = []
+coords.push({ lat: 321.213, long: 23.334 })
+coords.push({ x: 213, y: 43 })
 
 // Literal Types
-let zero: 0 = 0;
-let mood: "Happy" | "Sad" = "Happy";
-mood = "Sad";
+let zero: 0 = 0
+let mood: 'Happy' | 'Sad' = 'Happy'
+mood = 'Sad'
 
-type DayOfWeek =
-  | "Monday"
-  | "Tuesday"
-  | "Wednesday"
-  | "Thursday"
-  | "Friday"
-  | "Saturday"
-  | "Sunday";
+type DayOfWeek = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday'
 
-let today: DayOfWeek = "Sunday";
+let today: DayOfWeek = 'Sunday'
